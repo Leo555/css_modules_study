6fbd0b11bf8cc0483270b70c60c2a72a736700ff
LIZHEN2 <lizhen2@kingsoft.com>
2017-06-21 15:41:16 +0800

    add open-browser-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 module.exports = {
-  watch: true,
-  entry: __dirname + '/src/index.js',
-  output: {
-    publicPath: '/',
-    filename: './dist/bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['es2015', 'stage-0']
-      }
-    }, {
-      test: /\.css$/,
-      use: ["style-loader", {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            localIdentName: '[path][name]---[local]---[hash:base64:5]',
-            importLoaders: 1,
-            camelCase: true
-          }
-        }
-      }]
-    }]
-  }
+    watch: true,
+    entry: __dirname + '/src/index.js',
+    output: {
+        publicPath: '/',
+        filename: './dist/bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['es2015', 'stage-0']
+            }
+        }, {
+            test: /\.css$/,
+            use: ["style-loader", {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        localIdentName: '[path][name]---[local]---[hash:base64:5]',
+                        importLoaders: 1,
+                        camelCase: true
+                    }
+                }
+            }]
+        }]
+    },
+    plugins: [
+        new OpenBrowserPlugin({ url: 'index.html' })
+    ]
 };
